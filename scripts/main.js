b6463dd782f8624be1a2e8f34141137e9b2b3237
kevinlockett <chlockett@msn.com>
2024-02-07 15:46:02 -0600

    edited css

--- scripts/main.js
+++ scripts/main.js
@@ -5,22 +5,23 @@ import { Products } from "./Products.js"
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
-<h1>Brewed Awakenings</h1>
-<article class="details">
-    <section class="detail--column list details__employees">
-        <h2>Employees</h2>
-        ${Employees()}
-    </section>
-    <section class="detail--column list details__products">
-        <h2>Products</h2>
-        ${Products()}
-    </section>
-</article>
-
-<article class="orders">
-    <h1>Orders</h1>
-    ${Orders()}
-</article>
+    <header class="title">
+        <img src="../img/coffee.png" alt="Brewed Awakenings">
+    </header>
+    <article class="details">
+        <section class="detail--column list details__employees">
+            <h2>Employees</h2>
+            ${Employees()}
+        </section>
+        <section class="detail--column list details__products">
+            <h2>Products</h2>
+            ${Products()}
+        </section>
+    </article>
+    <article class="orders">
+        <h1>Orders</h1>
+        ${Orders()}
+    </article>
 `
 
 mainContainer.innerHTML = applicationHTML
